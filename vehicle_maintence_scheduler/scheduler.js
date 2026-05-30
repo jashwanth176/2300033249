e704b6f8cde836f8e42ba9a4137d3419760693ad
Jash <v.jashwanth.2005@gmail.com>
2026-05-30 11:39:32 +0530

    Added a maxImpact funtion which uses dp and backtracking to find the maximumm impact

---
+++ vehicle_maintence_scheduler/scheduler.js
@@ -0,0 +1,35 @@
+const SERVER_URL = process.env.SERVER_URL;
+const TOKEN = process.env.ACCESS_TOKEN;
+
+function maxImpact(hours, vehicles){
+    const n = vehicles.length;
+    const dp = Array(n+1).fill(0).map(() => Array(hours+1).fill(0));
+    for(let i = 1; i<=n; i++){
+        const v = vehicles[i-1];
+        const d = v.Duration;
+        const i = v.Impact;
+        for (let w = 0; w <= hours;w++){
+            if (d <= w)
+                dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-d]+i);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+    let res = dp[n][hours];
+    let w = hours;
+    const sel = [];
+    for (let i = n; i>0 && res > 0; i--){
+        if (res === dp[i-1][w]) continue;
+        else{
+            const v = vehicles[i-1];
+            sel.push(v.TaskID);
+            res -= v.Impact;
+            w -= v.Duration;
+        }
+    }
+    return {
+        maxImpact: dp[n][hours],
+        selectedIDs: sel
+    }
+}
+
