b3b1575a149b686c64658a5c30e8e84601ec6cc2
Jash <v.jashwanth.2005@gmail.com>
2026-05-30 12:21:11 +0530

    Vehicle Scheduler is finished, Made some bug fixes to the scheduler, added more logging and also local logging to check the output

--- vehicle_maintence_scheduler/scheduler.js
+++ vehicle_maintence_scheduler/scheduler.js
@@ -1,5 +1,7 @@
+require('dotenv').config({ path: '../.env' });
 const SERVER_URL = process.env.SERVER_URL;
 const TOKEN = process.env.ACCESS_TOKEN;
+const {sendLog} = require("../logging_middleware");
 
 function maxImpact(hours, vehicles){
     const n = vehicles.length;
@@ -7,10 +9,10 @@ function maxImpact(hours, vehicles){
     for(let i = 1; i<=n; i++){
         const v = vehicles[i-1];
         const d = v.Duration;
-        const i = v.Impact;
+        const imp = v.Impact;
         for (let w = 0; w <= hours;w++){
             if (d <= w)
-                dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-d]+i);
+                dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-d]+imp);
             else
                 dp[i][w] = dp[i-1][w];
         }
@@ -34,22 +36,31 @@ function maxImpact(hours, vehicles){
 }
 
 async function scheduleDepots(){
-    const headers = {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${TOKEN}`
-    };
-    const dRes = await fetch(`${SERVER_URL}/depots`, {headers});
-    const dData = await dRes.json();
-    const d = dData.depots;
-    const vRes = await fetch(`${SERVER_URL}/vehicles`, {headers});
-    const vData = await vRes.json();
-    const v = vData.vehicles;
+    try{
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${TOKEN}`
+        };
+        const dRes = await fetch(`${SERVER_URL}/depots`, {headers});
+        const dData = await dRes.json();
+        const d = dData.depots;
+        const vRes = await fetch(`${SERVER_URL}/vehicles`, {headers});
+        const vData = await vRes.json();
+        const v = vData.vehicles;
 
-    for (const depot of d){
-        const r = maxImpact(depot.MechanicHours, v);
-        const msg = `Depot ${depot.ID}: Max Impact = ${r.maxImpact}, Scheduled Tasks = [${r.selectedIDs.join(", ")}]`;
-        await sendLog("info", "scheduler", msg);
+        for (const depot of d){
+            const r = maxImpact(depot.MechanicHours, v);
+            const msg = `Depot ${depot.ID}: Max Impact = ${r.maxImpact}, Scheduled Tasks = [${r.selectedIDs.join(", ")}]`;
+            await sendLog("info", "service", msg);
+            console.log(msg);
+        }
+        await sendLog("info", "service", "Scheduling completed sucessfully");
+        console.log("Scheduling completed successfully");
+    }
+    catch (error){
+        await sendLog("error", "service", `Scheduling fail: ${error.message}`);
+        console.error("Scheduling failed:", error);
     }
 }
-
+scheduleDepots();
 module.exports = {scheduleDepots};
